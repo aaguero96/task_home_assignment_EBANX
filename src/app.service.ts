1af7dfd99c2871c0dafaa222064e2560d6e3c747
aaguero96 <andre.luiz_1996@hotmail.com>
2024-02-14 14:45:39 -0300

    feat: add config module

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly _config: ConfigService) {}
+
   getHello(): string {
-    return 'Hello World! 2';
+    return this._config.get('NODE_VERSION');
   }
 }
