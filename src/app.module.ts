e06724a4914af7808a14fa4c91bf973754b2b9be
Andre Luiz Aguero Domingos <87533012+aaguero96@users.noreply.github.com>
2024-02-16 15:03:45 -0300

    feat: publish image in dockerhub with gitactions
    * feat: try to publish image in dockerhub
    
    * refactor: change workflow
    
    * refactor: change workflow
    
    * refactor: change workflow
    
    * refactor: change workflow
    
    * refactor: change workflow
    
    * refactor: change workflow
    
    * refactor: change workflow
    
    * feat: add home page
    
    * refactor: change docker compose
    
    * refactor: change workflow

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { DBModule } from './infra/database/db.module';
 import { AccountRestModule } from './modules/account/rest/account-rest.module';
 import { SystemRestModule } from './modules/system/rest/system-rest.module';
+import { HomePageController } from './home-page.controlller';
 
 @Module({
   imports: [
@@ -14,5 +15,6 @@ import { SystemRestModule } from './modules/system/rest/system-rest.module';
     AccountRestModule,
     SystemRestModule,
   ],
+  controllers: [HomePageController],
 })
 export class AppModule {}
