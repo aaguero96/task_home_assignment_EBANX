74e055a3613324c181d1988bd3d1b77a49f0ce89
aaguero96 <andre.luiz_1996@hotmail.com>
2024-02-16 10:27:52 -0300

    feat: add port log

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './shared/filters/http-exception.filter';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalFilters(new HttpExceptionFilter());
 
-  await app.listen(3000);
+  const configService = app.get<ConfigService>(ConfigService);
+
+  const port = configService.get('PORT') || 3000;
+  await app.listen(port);
+  console.log(`App running on port: ${port}`);
 }
 bootstrap();
