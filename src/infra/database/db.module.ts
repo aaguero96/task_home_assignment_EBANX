7ef7b30827091499bb34f3abd7362ac9cefcc9b6
aaguero96 <andre.luiz_1996@hotmail.com>
2024-02-17 15:34:05 -0300

    refactor: change start of dbmodule

--- src/infra/database/db.module.ts
+++ src/infra/database/db.module.ts
@@ -11,7 +11,11 @@ import { join } from 'path';
         return {
           type: 'postgres',
           logging: configService.get('LOGGING_TYPEORM') == 'true',
-          url: configService.get('DATABASE_URL'),
+          host: configService.get('DATABASE_HOST'),
+          username: configService.get('DATABASE_USER'),
+          password: configService.get('DATABASE_PASSWORD'),
+          database: configService.get('DATABASE_NAME'),
+          ssl: configService.get('DATABASE_SSL') == 'true',
           entities: [join(__dirname, './entities/**/*.entity{.js,.ts}')],
           migrations: [join(__dirname, './migrations/**/*{.js,.ts}')],
           autoLoadEntities: true,
